4d6f95b2fc333f609c6dfd57126958244ac86d13
Egor Mashin <egor.mashin.04@mail.ru>
2023-11-24 11:05:43 +0700

    Добавьте файлы проекта.

---
+++ Model/BankCard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atm_mashinbank.Model {
+  public class BankCard {
+    public static string LastName { get; set; }
+    public static string FirstName { get; set; }
+    public static int PINCode { get; set; }
+    public static int BalanceCard { get; set; }
+    public static int WalletCash { get; set; }
+  }
+}
---
+++ Model/Singleton.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atm_mashinbank.Model {
+  internal class Singleton {
+  }
+}
